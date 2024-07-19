bae6699bee8df6eab9cba5afca665322192733cf
Dmitrii <provodin@bk.ru>
2024-07-19 07:57:18 +0300

    Редактировал всё пришлось обратится за помощью, сам ника не мог решить проблему!

--- src/masks.py
+++ src/masks.py
@@ -1,22 +1,18 @@
-def mask_card(card_number: str) -> str | None:
-    """Returns masked card number as string"""
-    if card_number.isdigit() and len(card_number) == 16:
-        return f"{card_number[:4]} {card_number[4:6]}{"*" * 2} {"*" * 4} {card_number[12:]}"
-    else:
-        return None
+def get_mask_card_number(card_num: str) -> str:
+    """Получаем номер карты, выводим скрытый номер карты"""
+    card_num_clear = card_num.split(" ")
+    hidden_num_list = (
+        card_num_clear[0][0:4],
+        (str(card_num_clear[0][4:6] + "**")),
+        "****",
+        card_num_clear[0][12:16],
+    )
+    hidden_num = " ".join(hidden_num_list)
+    return hidden_num
 
 
-def mask_account(acc_number: str) -> str | None:
-    """Returns masked account number as string"""
-    if acc_number.isdigit() and len(acc_number) == 20:
-        return f"{'*' * 2}{acc_number[-4::]}"
-    else:
-        return None
-
-
-def get_mask_account(nums):
-    return None
-
-
-def get_mask_card_number(param):
-    return None
+def get_mask_account(acc_num: str) -> str:
+    """Получаем номер аккаунта и выводим две звездочки и последние 4 цифры номера"""
+    hidden_num_list = "**", acc_num[-4:]
+    hidden_acc_num = " ".join(hidden_num_list)
+    return hidden_acc_num
--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,20 @@
-from src import masks
+def mask_account_card(acc_card_number: str) -> str:
+    """Функция скрытия номера карты или номера счета"""
+    if "Счет" in acc_card_number:
+        masked_info_list = "Счет ", "**", acc_card_number[-4:]
+        masked_info = "".join(masked_info_list)
+        return masked_info
+    num_index = ""
+    for symb in acc_card_number:
+        if symb.isdigit():
+            num_index = acc_card_number.find(symb)
+            break
+    if "Счет" not in acc_card_number:
+        bank_card = acc_card_number.replace(acc_card_number[(num_index + 6) : (num_index + 12)], "******")
+        return bank_card
 
 
-def mask_account_card(nums: str) -> str:
-    if 'Счёт' in nums:
-        return masks.get_mask_account(nums)
-    else:
-        cards = masks.get_mask_card_number(nums[-16:])
-        new_card = nums.replace(nums[-16:], cards)
-        return new_card
-
-
-print(mask_account_card(' Visa_Platinum 7000792289606361 '))
-
-
-def get_data(date: str) -> str:
-    return f'{date[8:10]}.{date[5:7]}.{date[0:4]}'
-
-
-print(get_data('2024-03-11T02:26:18.671407'))
+def get_date(system_date: str) -> str:
+    """Функция получения даты из системных данных"""
+    date = f"{system_date[8:10]}.{system_date[5:7]}.{system_date[0:4]}"
+    return date
